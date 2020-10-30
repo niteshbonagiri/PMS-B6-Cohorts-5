69b972c42dce65e8f821770a1f34bc3c1b521422
Nitesh kumar <nitesh.bonagiri@gmail.com>
2020-10-30 13:43:47 +0530

    Updated Database file

---
+++ PML-DataBase.sql
@@ -0,0 +1,139 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pml_db
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `backofficeuser`
+--
+
+LOCK TABLES `backofficeuser` WRITE;
+/*!40000 ALTER TABLE `backofficeuser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `backofficeuser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `commodities`
+--
+
+LOCK TABLES `commodities` WRITE;
+/*!40000 ALTER TABLE `commodities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commodities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `historicalrecordcommodity`
+--
+
+LOCK TABLES `historicalrecordcommodity` WRITE;
+/*!40000 ALTER TABLE `historicalrecordcommodity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historicalrecordcommodity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `historicalrecordcompany`
+--
+
+LOCK TABLES `historicalrecordcompany` WRITE;
+/*!40000 ALTER TABLE `historicalrecordcompany` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historicalrecordcompany` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `investorsharedetails`
+--
+
+LOCK TABLES `investorsharedetails` WRITE;
+/*!40000 ALTER TABLE `investorsharedetails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `investorsharedetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `recentlyviewedcompanies`
+--
+
+LOCK TABLES `recentlyviewedcompanies` WRITE;
+/*!40000 ALTER TABLE `recentlyviewedcompanies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recentlyviewedcompanies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `sharetransacction`
+--
+
+LOCK TABLES `sharetransacction` WRITE;
+/*!40000 ALTER TABLE `sharetransacction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sharetransacction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `stockprices`
+--
+
+LOCK TABLES `stockprices` WRITE;
+/*!40000 ALTER TABLE `stockprices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stockprices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `superuser`
+--
+
+LOCK TABLES `superuser` WRITE;
+/*!40000 ALTER TABLE `superuser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `superuser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `userwallet`
+--
+
+LOCK TABLES `userwallet` WRITE;
+/*!40000 ALTER TABLE `userwallet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userwallet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'pml_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-30 13:12:29
